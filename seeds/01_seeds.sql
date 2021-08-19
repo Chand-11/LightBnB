ae3d305b7ff8fe581738c38bace37fc309dfa905
chandni <chandnivaghasia11@gmail.com>
2021-08-18 22:48:59 -0400

    Add 01_queries set of 5 queries files

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -2,7 +2,7 @@ INSERT INTO users (name, email,password) VALUES
 ('ram nam','ram@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
 ('ratan tata','patan@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
 ('jatan birala','patan@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-('patan sarakar','patan@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+('patan sarakar','patan@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
 ('jamesh patel','jamesh_patel@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
 
@@ -14,17 +14,17 @@ VALUES(1, 'Hogwarts', 'description', 'https://imageurl', 'https://imageurl', 100
 (5, 'rowhouse', 'description', 'https://imageurl', 'https://imageurl', 60, 5, 2, 2, 'Canada', 'Bank st', 'Toronto', 'Ontario', 'A4G 4L5');
 
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
-VALUES ('2013-01-12', '2015-09-25', 6, 1),
- ('2019-10-11', '2020-10-26', 7, 5),
- ('2019-09-11', '2021-09-26', 8, 2),
- ('2021-09-11', '2024-09-26', 9, 3),
- ('2010-09-11', '2014-09-26', 10, 2);
+VALUES ('2013-01-12', '2015-09-25', 11, 1),
+ ('2019-10-11', '2020-10-26', 12, 5),
+ ('2019-09-11', '2021-09-26', 13, 2),
+ ('2021-09-11', '2024-09-26', 14, 3),
+ ('2010-09-11', '2014-09-26', 15, 2);
  
  INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
- VALUES(1, 7, 16, 5, 'message'),
- (2, 8, 17, 4, 'message'),
- (3, 10, 18, 3, 'message'),
- (2, 9, 19, 5, 'message'),
- (5, 6, 20, 5, 'message');
+ VALUES(1, 11, 11, 5, 'message'),
+ (2, 12, 12, 4, 'message'),
+ (3, 12, 13, 3, 'message'),
+ (2, 13, 13, 5, 'message'),
+ (5, 15, 14, 5, 'message');
 
 
